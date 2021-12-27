006a8797c030fb175d56a60ab0032188ebbeb1bb
phucdo <phucdo@freetekdemo.com>
2021-12-27 22:17:01 +0700

    updateStatistic

--- src/admin/statistic/page/StatisticPage.jsx
+++ src/admin/statistic/page/StatisticPage.jsx
@@ -8,14 +8,13 @@ import { getStatistic } from "../action";
 import { useSelector } from "react-redux";
 import { PieChart, Pie } from "recharts";
 import { Chart } from "react-google-charts";
-import {
-    DatePicker,
- 
-    Form,
-    
-  } from 'antd';
+import moment from "moment";
+import { DatePicker, Form } from "antd";
+import Loading from "../../../users/components/ui/Loading";
+import { ObjectUtils } from "../../../../src/utils/object.utils";
 
 import { useDispatch } from "react-redux";
+
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,40 +29,59 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#5FA3B7",
   },
   content: {
-   // display: "flex",
+    display: "flex",
     // marginLeft: theme.spacing(15),
   },
-  dateFrom:{
-   display: "flex",
-   marginLeft:theme.spacing(15),
-     justifyContent:"flex-start",
-    marginBottom :theme.spacing(1),
-    marginTop:theme.spacing(5), 
-  }
+  dateFrom: {
+    display: "flex",
+    marginLeft: theme.spacing(15),
+    justifyContent: "flex-start",
+    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(5),
+  },
 }));
 const StatisticPage = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const token = useSelector((state) => state.authAdmin.token);
-
+  const [startDate, setStartDate] = useState(moment());
+  const [endDate, setEndDate] = useState(moment());
   useEffect(() => {
     dispatch(
       getStatistic({
         token,
-        startAt: "2021-11-14T03:19:26.977Z",
-        endAt: "2021-12-18T03:19:26.977Z",
+        startAt: startDate.format(),
+        endAt: endDate.format(),
       })
     );
-  }, []);
+  }, [startDate, endDate]);
 
   const result = useSelector((state) => state.statistic.result);
-  console.log("result", result);
-  const data = [
-    { name: "Geeksforgeeks", students: 400 },
-    { name: "Technical scripter", students: 700 },
-    { name: "Geek-i-knack", students: 200 },
-    { name: "Geek-o-mania", students: 1000 },
-  ];
+  let temp = [];
+  let tempSale =[];
+  if (!result) {
+    <Loading />;
+  } else {
+    temp.push(["Product", "Total Quantity"]);
+    tempSale.push(["Product","Total totalSale"]);
+    let statistic = result.statistic;
+    statistic.forEach((item) => temp.push([item.name, item.totalQuantity]));
+    statistic.forEach((item) => tempSale.push([item.name, item.totalSale]));
+    
+    const otherQuantity = result.statistic.reduce((sum, item) => {
+      return sum + item.totalQuantity;
+    }, 0);
+
+    const otherSale = result.statistic.reduce((sum, item) => {
+      return sum + item.totalSale;
+    }, 0);
+
+    const otherTotalQuantity = result.totalQuanity - otherQuantity;
+    const otherTotalSale =result.totalSale -otherSale;
+    temp.push(["other", otherTotalQuantity]);
+    tempSale.push(["other", otherTotalSale]);
+  }
+
   return (
     <Layout>
       <div className={classes.root}>
@@ -80,9 +98,10 @@ const StatisticPage = () => {
               rules={[{ required: true, message: "Please choose a startAt!" }]}
             >
               <DatePicker
-              label ="startAt"
+                label="startAt"
                 placeholder="Select date"
-                
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
               />
             </Form.Item>
             <Form.Item
@@ -91,31 +110,46 @@ const StatisticPage = () => {
               rules={[{ required: true, message: "Please choose a endAt!" }]}
             >
               <DatePicker
-              label ="startAt"
-            placeholder="Select date"
+                label="endAt"
+                placeholder="Select date"
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
               />
             </Form.Item>
           </div>
           <div className={classes.content}>
-          <Chart
-            width={"800px"}
-            height={"500px"}
-            chartType="PieChart"
-            loader={<div>Loading Chart</div>}
-            data={[
-              ["Task", "Hours per Day"],
-              ["Work", 11],
-              ["Eat", 2],
-              ["Commute", 2],
-              ["Watch TV", 2],
-              ["Sleep", 7],
-            ]}
-            options={{
-              title: "Best Sale Product",
-            }}
-            rootProps={{ "data-testid": "1" }}
-          />
-        </div>
+            <Chart
+              width={"500px"}
+              height={"400px"}
+              chartType="PieChart"
+              loader={<div>Loading Chart</div>}
+              // data={[
+              //   ["Task", "Hours per Day"],
+              //   ["Work", 11],
+              //   ["Eat", 2],
+              //   ["Commute", 2],
+              //   ["Watch TV", 2],
+              //   ["Sleep", 7],
+              // ]}
+              data={temp}
+              options={{
+                title: "Sản phẩm bán được số lượng nhiều nhất",
+              }}
+              rootProps={{ "data-testid": "1" }}
+            />
+
+            <Chart
+              width={"500px"}
+              height={"400px"}
+              chartType="PieChart"
+              loader={<div>Loading Chart</div>}
+               data={tempSale}
+              options={{
+                title: "Sản phẩm có doanh số cao nhất",
+              }}
+              rootProps={{ "data-testid": "1" }}
+            />
+          </div>
         </div>
       </div>
     </Layout>
